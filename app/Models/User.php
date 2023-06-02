bf5706833992f56d9077d4e58e66962a91475d6d
luthfeew <luthfeew@gmail.com>
2023-06-02 18:36:34 +0700

    User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,8 +19,10 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'email',
+        // 'email',
+        'username',
         'password',
+        'role',
     ];
 
     /**
